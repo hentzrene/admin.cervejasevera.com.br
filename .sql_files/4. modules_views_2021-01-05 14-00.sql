a457817b9b5c6b6e49e0decfd84bde4ec69e4433
sneverton <ton.fera.cotri@gmail.com>
2021-01-27 17:29:11 -0400

    sql files fix

--- .sql_files/4. modules_views_2021-01-05 14-00.sql
+++ .sql_files/4. modules_views_2021-01-05 14-00.sql
@@ -1,6 +1,6 @@
 /*!40000 ALTER TABLE `modules_views` DISABLE KEYS */;
 INSERT INTO `modules_views` (`id`, `name`, `key`) VALUES
 	(1, 'Customizado', 'custom'),
-	(2, 'Tabela', 'table');
+	(2, 'Tabela', 'table'),
 	(3, 'Item', 'item');
 /*!40000 ALTER TABLE `modules_views` ENABLE KEYS */;
