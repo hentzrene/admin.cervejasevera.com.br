a19c1022636f2783e61c62195fcda4841e926337
sneverton <ton.fera.cotri@gmail.com>
2021-01-28 11:07:07 -0400

    update

--- .sql_files/5. modules_2021-01-05 14-00.sql
+++ .sql_files/5. modules_2021-01-05 14-00.sql
@@ -1,5 +1,4 @@
 /*!40000 ALTER TABLE `modules` DISABLE KEYS */;
 INSERT INTO `modules` (`id`, `name`, `key`, `icon`, `modules_views_id`, `view_options`, `active`, `removable`) VALUES
-	(1, 'Contas', 'accounts', 'fas fa-users', 1, NULL, 1, 0),
-	(2, 'Módulos', 'modules', 'fas fa-cogs', 1, NULL, 1, 0);
+	(1, 'Contas', 'accounts', 'fas fa-users', 1, NULL, 1, 0);
 /*!40000 ALTER TABLE `modules` ENABLE KEYS */;
