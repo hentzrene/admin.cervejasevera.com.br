88c95dd1cb34aeccf93731607f6bd7e773c395b5
sneverton <ton.fera.cotri@gmail.com>
2021-03-31 17:38:29 -0400

    fix

--- server/Core/Routers.php
+++ server/Core/Routers.php
@@ -2,5 +2,4 @@
 require __DIR__ . '/Routers/Modules.php';
 require __DIR__ . '/Routers/System.php';
 require __DIR__ . '/Routers/Configuration.php';
-require __DIR__ . '/Routers/Email.php';
 require __DIR__ . '/Routers/Setup.php';
