1c3121d5de4f18f5e700f0feed1792044e4bbd89
sneverton <ton.fera.cotri@gmail.com>
2021-05-29 11:31:51 -0400

    Revert "[SYNC]"
    This reverts commit 4e50d887b07b4fcc553a7fd7ce598b95a177b951.

--- server/Custom/Controller/Content.php
+++ server/Custom/Controller/Content.php
@@ -12,175 +12,13 @@ class Content
 {
   public function getAllInformations()
   {
-    $item = new Item(
+    $Item = new Item(
       'informations',
-      ['img', 'name', 'local', 'tel', 'email', 'operation', 'map'],
+      ['img', 'name'],
       1
     );
 
-    Response::rawBody($item->data);
-
-    Response::send();
-  }
-
-  public function getAllSlides()
-  {
-    $collection = new Collection(
-      'slides',
-      ['title', 'link',  'desktop', 'mobile'],
-    );
-
-    Response::rawBody($collection->data);
-
-    Response::send();
-  }
-
-  public function getAllEducation()
-  {
-    $collection = new Collection(
-      'education',
-      ['id', 'title'],
-    );
-
-    Response::rawBody($collection->data);
-
-    Response::send();
-  }
-
-  public function getEducation($d)
-  {
-    $item = new Item(
-      'education',
-      ['id', 'title', 'text', 'img as mainImg'],
-      (int) $d['itemId']
-    );
-
-    $item->withImages('img');
-
-    Response::rawBody($item->data);
-
-    Response::send();
-  }
-
-  public function getJeanPiaget()
-  {
-    $item = new Item(
-      'jean_piaget',
-      ['intro', 'text', 'img'],
-      1
-    );
-
-    Response::rawBody($item->data);
-
-    Response::send();
-  }
-
-  public function getAllVideos()
-  {
-    $collection = new Collection(
-      'videos',
-      ['id', 'title', 'link'],
-      new CollectionInit([
-        'page' => (int) Req::get('page'),
-        'itemsPerPage' => (int) Req::get('itemsPerPage'),
-        'returnTotalItems' => (int) Req::get('returnTotalItems')
-      ])
-    );
-
-    Response::rawBody($collection->data);
-
-    Response::send();
-  }
-
-  public function getVideo($d)
-  {
-    $item = new Item(
-      'videos',
-      ['title', 'text', 'link', 'source'],
-      (int) $d['itemId']
-    );
-
-    Response::rawBody($item->data);
-
-    Response::send();
-  }
-
-  public function getAllProjects()
-  {
-    $collection = new Collection(
-      'projects',
-      [
-        'id',
-        'title',
-        'SUBSTRING(text, 1, 70) as text',
-        'img'
-      ],
-      new CollectionInit([
-        'page' => (int) Req::get('page'),
-        'itemsPerPage' => (int) Req::get('itemsPerPage'),
-        'returnTotalItems' => (int) Req::get('returnTotalItems')
-      ])
-    );
-
-    Response::rawBody($collection->data);
-
-    Response::send();
-  }
-
-  public function getAllGalleries()
-  {
-    $collection = new Collection(
-      'galleries',
-      [
-        'id',
-        'title',
-        'img'
-      ],
-      new CollectionInit([
-        'page' => (int) Req::get('page'),
-        'itemsPerPage' => (int) Req::get('itemsPerPage'),
-        'returnTotalItems' => (int) Req::get('returnTotalItems')
-      ])
-    );
-
-    Response::rawBody($collection->data);
-
-    Response::send();
-  }
-
-  public function getGallery($d)
-  {
-    $item = new Item('galleries', [
-      'id',
-      'title',
-      'date_start',
-      'text'
-    ], (int) $d['itemId']);
-
-    $item->withImages('img');
-
-    Response::rawBody($item->data);
-
-    Response::send();
-  }
-
-  public function getAllCommunications()
-  {
-    $collection = new Collection(
-      'communications',
-      [
-        'id',
-        'title',
-        'scheduled'
-      ],
-      new CollectionInit([
-        'page' => (int) Req::get('page'),
-        'itemsPerPage' => (int) Req::get('itemsPerPage'),
-        'returnTotalItems' => (int) Req::get('returnTotalItems')
-      ])
-    );
-
-    Response::rawBody($collection->data);
+    Response::rawBody($Item->data);
 
     Response::send();
   }
@@ -210,7 +48,7 @@ class Content
 
   public function getArticle($d)
   {
-    $item = new Item('articles', [
+    $Item = new Item('articles', [
       'id',
       'title',
       'source',
@@ -220,62 +58,11 @@ class Content
       'text'
     ], (int) $d['itemId']);
 
-    $item->withImages('img');
+    $Item->withImages('img');
 
-    $item->withAttachments('file');
+    $Item->withAttachments('file');
 
-    Response::rawBody($item->data);
-
-    Response::send();
-  }
-
-  public function getWebCanal()
-  {
-    $item = new Item(
-      'webcanal',
-      ['text', 'img', 'link'],
-      1
-    );
-
-    Response::rawBody($item->data);
-
-    Response::send();
-  }
-
-  public function getFeaturedVideo()
-  {
-    $item = new Item(
-      'featured_video',
-      ['title', 'text', 'link'],
-      1
-    );
-
-    Response::rawBody($item->data);
-
-    Response::send();
-  }
-
-  public function getAllQuickAccess()
-  {
-    $collection = new Collection(
-      'quick_access',
-      ['title', 'img', 'link'],
-      new CollectionInit(['order' => 'ASC'])
-    );
-
-    Response::rawBody($collection->data);
-
-    Response::send();
-  }
-
-  public function getAllCovid()
-  {
-    $collection = new Collection(
-      'covid',
-      ['title',  'file', 'source', 'scheduled']
-    );
-
-    Response::rawBody($collection->data);
+    Response::rawBody($Item->data);
 
     Response::send();
   }
--- server/Custom/Routers.php
+++ server/Custom/Routers.php
@@ -3,21 +3,6 @@ $router->namespace('Custom\Controller');
 $router->group('rest');
 
 $router->get('/informations', 'Content:getAllInformations');
-$router->get('/slides', 'Content:getAllSlides');
-$router->get('/education', 'Content:getAllEducation');
-$router->get('/education/{itemId}', 'Content:getEducation');
-$router->get('/quick-access', 'Content:getAllQuickAccess');
-$router->get('/featured-video', 'Content:getFeaturedVideo');
-$router->get('/webcanal', 'Content:getWebCanal');
-$router->get('/articles', 'Content:getAllArticles');
 $router->get('/articles/{itemId}', 'Content:getArticle');
-$router->get('/communications', 'Content:getAllCommunications');
-$router->get('/galleries', 'Content:getAllGalleries');
-$router->get('/galleries/{itemId}', 'Content:getGallery');
-$router->get('/projects', 'Content:getAllProjects');
-$router->get('/videos', 'Content:getAllVideos');
-$router->get('/videos/{itemId}', 'Content:getVideo');
-$router->get('/jean-piaget', 'Content:getJeanPiaget');
-$router->get('/covid', 'Content:getAllCovid');
 
 $router->post('/email/contact-us', 'Email:contactUs');
