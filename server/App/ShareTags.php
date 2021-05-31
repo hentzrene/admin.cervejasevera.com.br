9c9af8c58d24e76fab7a70e1a138fbdc30d6b285
sneverton <ton.fera.cotri@gmail.com>
2021-05-31 16:07:41 -0400

    [UPD] Adicionado rotas GET para as páginas do site.

--- server/App/ShareTags.php
+++ server/App/ShareTags.php
@@ -25,7 +25,97 @@ class ShareTags
 
     return [
       '/' => [
-        'title' => 'MRX Websites - ' . self::$defaultTags->name,
+        'title' => 'Início - ' . self::$defaultTags->name
+      ],
+      '/institucional/o-colegio' => [
+        'title' => 'O Colégio - ' . self::$defaultTags->name
+      ],
+      '/institucional/equipe' => [
+        'title' => 'Equipe - ' . self::$defaultTags->name
+      ],
+      '/institucional/quem-foi-jean-piaget' => [
+        'title' => 'Quem foi Jean Piaget? - ' . self::$defaultTags->name
+      ],
+      '/institucional/proposta-pedagogica' => [
+        'title' => 'Proposta Pedagógica - ' . self::$defaultTags->name
+      ],
+      '/ensino/{item}' => [
+        'title' => 'Ensino - ' . self::$defaultTags->name
+      ],
+      '/ensino/projetos' => [
+        'title' => 'Projetos - ' . self::$defaultTags->name
+      ],
+      '/ensino/projetos/{item}' => [
+        'title' => 'Projetos - ' . self::$defaultTags->name
+      ],
+      '/novidades/noticias' => [
+        'title' => 'Notícias - ' . self::$defaultTags->name
+      ],
+      '/novidades/noticias/{item}' => [
+        'title' => 'Notícias - ' . self::$defaultTags->name
+      ],
+      '/novidades/galerias' => [
+        'title' => 'Galerias - ' . self::$defaultTags->name
+      ],
+      '/novidades/galerias/{item}' => [
+        'title' => 'Galerias - ' . self::$defaultTags->name
+      ],
+      '/novidades/videos' => [
+        'title' => 'Vídeos - ' . self::$defaultTags->name
+      ],
+      '/novidades/videos/{item}' => [
+        'title' => 'Vídeos - ' . self::$defaultTags->name
+      ],
+      '/novidades/comunicados' => [
+        'title' => 'Comunicados da Escola - ' . self::$defaultTags->name
+      ],
+      '/novidades/comunicados/{item}' => [
+        'title' => 'Comunicados da Escola - ' . self::$defaultTags->name
+      ],
+      '/aluno/atividades-e-compromissos' => [
+        'title' => 'Atividades e Compromissos - ' . self::$defaultTags->name
+      ],
+      '/aluno/atividades-e-compromissos/{item}' => [
+        'title' => 'Atividades e Compromissos - ' . self::$defaultTags->name
+      ],
+      '/aluno/links-uteis' => [
+        'title' => 'Links Úteis - ' . self::$defaultTags->name
+      ],
+      '/aluno/dicas-interessantes' => [
+        'title' => 'Dicas Interessantes - ' . self::$defaultTags->name
+      ],
+      '/aluno/licao-de-casa' => [
+        'title' => 'Lição de Casa - ' . self::$defaultTags->name
+      ],
+      '/aluno/licao-de-casa/{item}' => [
+        'title' => 'Lição de Casa - ' . self::$defaultTags->name
+      ],
+      '/horario-de-aulas' => [
+        'title' => 'Horário de Aulas - ' . self::$defaultTags->name
+      ],
+      '/carta-aos-pais' => [
+        'title' => 'Carta aos Pais - ' . self::$defaultTags->name
+      ],
+      '/destaques' => [
+        'title' => 'Destaques - ' . self::$defaultTags->name
+      ],
+      '/contato/fale-conosco' => [
+        'title' => 'Fale Conosco - ' . self::$defaultTags->name
+      ],
+      '/contato/trabalhe-conosco' => [
+        'title' => 'Trabalhe Conosco - ' . self::$defaultTags->name
+      ],
+      '/leituras' => [
+        'title' => 'Leituras - ' . self::$defaultTags->name
+      ],
+      '/pre-matricula' => [
+        'title' => 'Pré Matrícula - ' . self::$defaultTags->name
+      ],
+      '/boletim-corona-virus' => [
+        'title' => 'Boletim Corona Vírus - ' . self::$defaultTags->name
+      ],
+      '/error404' => [
+        'title' => 'Erro 404 - ' . self::$defaultTags->name
       ],
     ];
   }
