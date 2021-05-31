7705f85b4bffcae1ac2a61034ccd6f49047d538b
sneverton <ton.fera.cotri@gmail.com>
2021-05-31 10:28:58 -0400

    [UPD] Melhorado a forma de obter os dados.

--- server/Custom/Model/Collection.php
+++ server/Custom/Model/Collection.php
@@ -2,7 +2,8 @@
 
 namespace Custom\Model;
 
-use Core\Model\Utility\Conn;
+use Custom\Model\Conn\Conn;
+use Custom\Model\CollectionInit\CollectionInit;
 
 class Collection
 {
@@ -27,17 +28,51 @@ class Collection
 
     $limit = $init->itemsPerPage > self::MAX_ITEMS_PER_PAGE || !$init->itemsPerPage ? self::MAX_ITEMS_PER_PAGE  : $init->itemsPerPage;
 
+    $columnsWithTable = [];
+    foreach ($columns as $k => $v) {
+      if (!is_int($k)) {
+        $columnsWithTable["mod_$moduleKey.$k"] = $v;
+      } else {
+        $columnsWithTable[$k] = "mod_$moduleKey.$v";
+      }
+    }
+
+    if ($init->join) {
+      foreach ($init->join->items as $joinItem) {
+        if ($joinItem->type === 'categories') {
+          $columnsWithTable[] = "categories.id as {$joinItem->fieldKey}_id";
+          $columnsWithTable[] = "categories.title as {$joinItem->fieldKey}_title";
+        }
+      }
+    }
+
     $q = Conn::table("mod_$moduleKey")
-      ::select($columns)
-      ::where('active', 1)
-      ::and('IF(showFrom, CURRENT_TIMESTAMP > showFrom, TRUE)', 1)
-      ::and('IF(showUp, CURRENT_TIMESTAMP < showUp, TRUE)', 1)
-      ::orderBy('id', $init->order ? $init->order : 'DESC')
+      ::select($columnsWithTable);
+
+    if ($init->join) {
+      foreach ($init->join->items as $joinItem) {
+        $q = $q::leftJoin($joinItem->type);
+      }
+
+      $i = 0;
+      foreach ($init->join->items as $joinItem) {
+        if ($i === 0) {
+          $q = $q::on("mod_$moduleKey.$joinItem->fieldKey", "$joinItem->type.id");
+        } else {
+          $q = $q::and("mod_$moduleKey.$joinItem->fieldKey", "$joinItem->type.id");
+        }
+        $i++;
+      }
+    }
+
+    $q = $q::where('active', 1)
+      ::and("IF(mod_$moduleKey.showFrom, CURRENT_TIMESTAMP > mod_$moduleKey.showFrom, TRUE)", 1)
+      ::and("IF(mod_$moduleKey.showUp, CURRENT_TIMESTAMP < mod_$moduleKey.showUp, TRUE)", 1)
+      ::orderBy("mod_$moduleKey.id", $init->order ? $init->order : 'DESC')
       ::limit($limit, $offset)
       ::send();
 
     if (!$q) throw new \Exception('Não foi possível obter os dados da coleção.');
-
 
     $list = $q->fetch_all(MYSQLI_ASSOC);
     if (!$init->returnTotalItems) {
