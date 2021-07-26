9bd62b6fe35c4315382aa4636670920ec8423f4b
sneverton <everton@andrade.mat.br>
2021-07-26 15:05:55 -0400

    feat: agora quando não tiver imagem, aparecerá nada

--- .vue-admin/src/modules/fields/components/image-file/formatForDisplay.js
+++ .vue-admin/src/modules/fields/components/image-file/formatForDisplay.js
@@ -1,7 +1,9 @@
 export default async ({ component: { files }, value }) => {
+  if (!value) return;
+
   const src = files.replace("/admin", "") + value + "?resize=1&w=106";
 
   const img = `<img style="height: 60px; width: 106px; object-fit: cover; margin-top: 6px; border-radius: 4px;" src="${src}">`;
 
-  return Promise.resolve({ innerHTML: img });
+  return { innerHTML: img };
 };
