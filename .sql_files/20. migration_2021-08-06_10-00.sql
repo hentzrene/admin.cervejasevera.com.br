987cef86c5d7ccde3be67763c783335b77316507
sneverton <everton@andrade.mat.br>
2021-08-06 10:08:59 -0400

    feat: adicionado campo Cor

---
+++ .sql_files/20. migration_2021-08-06_10-00.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+  `modules_fields_types` (
+    `id`,
+    `name`,
+    `key`,
+    `sql_type`
+  )
+VALUES
+  (
+    22,
+    'Cor',
+    'color',
+    'CHAR(9)'
+  );
---
+++ .vue-admin/src/modules/fields/components/color/Index.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+grid-item(col-end=2, col-end-sm=1).d-flex
+  v-menu(offset-y :close-on-content-click="false")
+    template(#activator="{ on, attrs }")
+      v-btn(v-bind="attrs" v-on="on" :color="value_" fab depressed x-small).mt-1.mr-2
+        v-icon fas fa-tint
+    v-color-picker(v-model="value_" mode="hexa" dark)
+  v-text-field(
+    :label="label",
+    :value="value_",
+    :name="name",
+    readonly
+    dense,
+    outlined,
+    dark
+  )
+</template>
+
+<script>
+import mixin from "../../mixin";
+
+export default {
+  data: () => ({
+    picker: false,
+    value_: null,
+  }),
+  mounted() {
+    this.value_ = this.value;
+  },
+  watch: {
+    value(v) {
+      this.value_ = v;
+    },
+  },
+  mixins: [mixin],
+};
+</script>
