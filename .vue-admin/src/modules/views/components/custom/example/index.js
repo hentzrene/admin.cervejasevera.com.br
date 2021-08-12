33a7e94da261ab1c141374965940465b7f8536fc
sneverton <everton@andrade.mat.br>
2021-08-12 16:32:11 -0400

    chore

---
+++ .vue-admin/src/modules/views/components/custom/example/index.js
@@ -0,0 +1,26 @@
+import List from "./List";
+import Add from "./Add";
+import Item from "./Item";
+
+const key = "example";
+
+export default {
+  key,
+  routes: [
+    {
+      name: "Exemplos",
+      path: `/${key}`,
+      component: List
+    },
+    {
+      name: "Adicionar exemplo",
+      path: `/${key}/adicionar`,
+      component: Add
+    },
+    {
+      name: "Alterar exemplo",
+      path: `/${key}/:example`,
+      component: Item
+    }
+  ]
+};
--- .vue-admin/src/modules/views/components/custom/index-example.js
+++ .vue-admin/src/modules/views/components/custom/index-example.js
@@ -1,34 +1,3 @@
-// example.js
-import List from "./List";
-import Add from "./Add";
-import Item from "./Item";
-
-const key = "example";
-
-export default {
-  key,
-  routes: [
-    {
-      name: "Exemplos",
-      path: `/${key}`,
-      component: List
-    },
-    {
-      name: "Adicionar exemplo",
-      path: `/${key}/adicionar`,
-      component: Add
-    },
-    {
-      name: "Alterar exemplo",
-      path: `/${key}/:example`,
-      component: Item
-    }
-  ]
-};
-
-/******************************************/
-
-// index.js
 import example from "./example";
 
 export default [example];
