981fd6525558ebfc433684c388ca5af0dd3672f1
sneverton <everton@andrade.mat.br>
2021-08-18 17:14:12 -0400

    feat: alterado página inicial do painel

--- .vue-admin/src/components/pages/Home.vue
+++ .vue-admin/src/components/pages/Home.vue
@@ -8,14 +8,36 @@ v-sheet.mx-auto.pa-4.d-flex.align-center(
   .pa-4.white--text.font-weight-bold.primary.rounded-pill.text-body-1.mx-auto(
     v-else-if="!modules.length"
   ) Nenhum módulo adicionado.
-  grid-list.py-4(v-else, col-width="150px", gap="16px")
-    v-responsive(
-      v-for="({ name, icon, key }, i) in modules",
-      :aspect-ratio="1",
-      :key="i",
-      width="150px"
+  .mx-auto(v-else)
+    grid-container.py-4(
+      cols="150px 150px",
+      cols-sm="repeat(4, 150px)",
+      cols-lg="repeat(5, 150px)",
+      gap="16px"
     )
-      module-card(:to="'/' + key", :title="name", :icon="icon")
+      template(v-for="({ text, to, icon, items }, i) in nav")
+        v-responsive(v-if="!items", :aspect-ratio="1", :key="i", width="150px")
+          module-card(:to="to", :title="text", :icon="icon")
+        grid-item(
+          v-else,
+          col-end="span 2",
+          col-end-sm="span 4",
+          col-end-lg="span 5"
+        )
+          .white--text.text-body-1.text-uppercase.font-weight-bold {{ text }}
+          grid-container(
+            cols="100px 100px 100px",
+            cols-sm="repeat(5, 120px)",
+            cols-lg="repeat(6, 125px)",
+            gap="16px"
+          )
+            v-responsive(
+              v-for="({ text, to, icon }, i) in items",
+              :aspect-ratio="1",
+              :key="i",
+              width="125"
+            )
+              module-card(:to="to", :title="text", :icon="icon")
 </template>
 
 <script>
@@ -27,6 +49,45 @@ export default {
   computed: {
     modules() {
       return this.$rest("modules").list;
+    },
+    nav() {
+      let withMenu = [];
+      let withoutMenu = [];
+
+      for (let m of this.modules)
+        if (m.menuId) withMenu.push(m);
+        else withoutMenu.push(m);
+
+      withMenu = withMenu.reduce((r, v) => {
+        const menu = r.find(({ text }) => text === v.menuTitle);
+
+        if (!menu) {
+          r.push({
+            text: v.menuTitle,
+            items: [{ text: v.name, to: "/" + v.key, icon: v.icon }],
+          });
+        } else {
+          menu.items.push({ text: v.name, to: "/" + v.key, icon: v.icon });
+        }
+
+        return r;
+      }, []);
+
+      withoutMenu = withoutMenu.map(({ name, key, icon }) => ({
+        text: name,
+        to: "/" + key,
+        icon,
+      }));
+
+      withoutMenu = withoutMenu.sort((a, b) => a.text.localeCompare(b.text));
+      withMenu = withMenu
+        .map((menu) => {
+          menu.items = menu.items.sort((a, b) => a.text.localeCompare(b.text));
+          return menu;
+        })
+        .sort((a, b) => a.text.localeCompare(b.text));
+
+      return [...withoutMenu, ...withMenu];
     },
   },
   beforeCreate() {
