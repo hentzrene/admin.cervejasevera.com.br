c9961a80fc3ae39a06c01fd4e0f6d5bd01b7e0f1
sneverton <everton@andrade.mat.br>
2021-10-26 10:09:46 -0400

    feat: criado a função beforeTableExport do campo Collection

--- server/Module/Field/Collection/Model.php
+++ server/Module/Field/Collection/Model.php
@@ -4,4 +4,15 @@ namespace Module\Field\Collection;
 
 class Model
 {
+  public static function beforeTableExport(string $moduleKey, $fieldData, object $options, array $list): array
+  {
+    $list = array_map(function ($c) use ($fieldData) {
+      if (!is_null($c[$fieldData['key']])) {
+        $c[$fieldData['key']] = $c[$fieldData['key']] ? implode(', ', json_decode($c[$fieldData['key']]))  : '';
+      }
+      return $c;
+    }, $list);
+
+    return $list;
+  }
 }
