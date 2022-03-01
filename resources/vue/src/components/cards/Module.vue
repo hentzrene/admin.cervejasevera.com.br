4316e37f6d99eba08bf5eef51f38f35e76de1303
Everton Andrade <everton@andrade.mat.br>
2022-03-01 11:24:35 -0400

    fix

--- resources/vue/src/components/cards/Module.vue
+++ resources/vue/src/components/cards/Module.vue
@@ -1,10 +1,5 @@
 <template lang="pug">
-v-card.module-card.flex-column.justify-center(
-  :to="to",
-  height="100%",
-  color="primary lighten-1",
-  outlined
-)
+v-card.module-card(:to="to", height="100%", color="primary lighten-1", outlined)
   v-icon(size="50", color="grey lighten-3") {{ icon }}
   span.font-weight-medium.mt-2.grey--text.text--lighten-5.text-center.module-card-title {{ title }}
 </template>
@@ -30,12 +25,12 @@ export default {
 
 <style>
 .module-card {
-  display: grid;
-  grid-template-rows: 60px max-content;
-  justify-content: center;
-  align-content: center;
+  display: grid !important;
+  grid-template-rows: 60px max-content !important;
+  justify-content: center !important;
+  align-content: center !important;
 }
 .module-card-title {
-  white-space: break-spaces;
+  white-space: break-spaces !important;
 }
 </style>
