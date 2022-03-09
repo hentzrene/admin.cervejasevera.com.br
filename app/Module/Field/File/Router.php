b0c67704115a9ac64f05ef50406045d6fce05c8b
MRX Web Sites <61802167+MRXWeb@users.noreply.github.com>
2022-03-09 08:34:05 -0400

    fix: não estava atualizando o título dos arquivos do campo File

--- app/Module/Field/File/Router.php
+++ app/Module/Field/File/Router.php
@@ -5,5 +5,5 @@ $router->namespace('Module\Field\File');
 
 $router->get('/rest/modules-files/{itemId}', 'Controller:getAllItems');
 $router->post('/rest/modules-files/file', 'Controller:addItem');
-$router->put('/rest/modules-files/{fileId}/title', 'Controller:setTitleItem');
+$router->put('/rest/modules-files/{fileId}/title', 'Controller:setItemTitle');
 $router->delete('/rest/modules-files/{fileId}', 'Controller:removeItem');
